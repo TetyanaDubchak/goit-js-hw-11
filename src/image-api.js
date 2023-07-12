a411f1108046cedfdb2e22f7f46cbfb35613b30a
TetyanaDubchak <123674629+TetyanaDubchak@users.noreply.github.com>
2023-07-12 18:03:02 +0300

    dont work nothing

--- src/image-api.js
+++ src/image-api.js
@@ -1,8 +1,9 @@
-const axios = require('axios/dist/node/axios.cjs');
+import axios from "axios";
 
 const URL_BASE = 'https://pixabay.com/api/';
 const API_KEY = '38155238-6cbc32329127063edf5d1a6f9'
 
+let page = 1;
 // function getImage() {
 //     fetch(`${URL_BASE}?key=${API_KEY}&q=cat&image_type=photo&orientation=horizontal&safesearch=true`).then(resp => {
 //         if (!resp.ok) {
@@ -12,15 +13,12 @@ const API_KEY = '38155238-6cbc32329127063edf5d1a6f9'
 //     });
 // };
 
-async function getImage() {
-  try {
-    const response = await axios.get(`${URL_BASE}?key=${API_KEY}&q=cat&image_type=photo&orientation=horizontal&safesearch=true`);
-    console.log(response);
-  } catch (error) {
-    console.error(error);
-  }
+async function getImage(input) {
+ 
+    return await axios.get(`${URL_BASE}?key=${API_KEY}&q=${input}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
+  
 }
 
-export { getImage }
+export { getImage, page }
 
 // ${URL_BASE}?key=${API_KEY}&q=${userWord}&image_type=photo&orientation=horizontal&safesearch=true
--- src/index.js
+++ src/index.js
@@ -1,23 +1,89 @@
 import Notiflix from 'notiflix';
 
 
-import { getImage } from "./image-api";
+import { getImage, page } from "./image-api";
 
 const formEl = document.querySelector('.search-form');
 const buttonEl = document.querySelector('.search-form button');
-const inputEl = document.querySelector('.search-form input');
-console.dir(inputEl);
+const galleryEl = document.querySelector('.gallery');
+const loadMoreBtnEl = document.querySelector('.load-more');
+
+console.log(getImage(cat));
+
+const { searchQuery } = formEl.elements;
+const inputValue = searchQuery.value;
+
+
+const onFormSubmitHandler = async evt => {
+    evt.preventDefault();
+
+  try {
+    const { obj } = getImage(inputValue);
+    createMarkUp(obj);
+    galleryEl.innerHTML = markUpImage;
+    
+    loadMoreBtnEl.classList.remove('is-hidden');
+  }
+  
+  catch {
+    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+  }
+  
+    // getImage(inputValue).then(obj => {
+    //     console.log(obj)
+    //     createMarkUp(obj);
+    // }).catch(e => {
+    //   Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    // console.error(e)
+    // })
+}
+
+function createMarkUp(obj) {
+    const makePhotoMark = ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+    return `<div class="photo-card">
+  <img class='photo-img' src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b> Likes</b> <span>${likes}</span>
+    </p>
+    <p class="info-item">
+      <b> Views </b> <span>${views}</span>
+    </p>
+    <p class="info-item">
+      <b> Comments </b> <span> ${comments}</span>
+    </p>
+    <p class="info-item">
+      <b> Downloads </b> <span>${downloads}</span>
+    </p>
+  </div>
+</div>`
+    };
+  const arrayData = obj.data.hits;
+  const markUpImage = arrayData.map(item => makePhotoMark(item)).join('');
+    
+}
+
+const onLoadMoreHandler = async () => {
+  page += 1;
+
+  try {
+      const { obj } = getImage(inputValue);
+      
+    if(page === obj.data.totalHits) {
+      loadMoreBtnEl.classList.remove('is-hidden');
+      Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
+    }
+    
+    galleryEl.insertAdjacentHTML('beforeend', createMarkUp(obj))
+
+  } catch {
+       Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+  }
+
+}
+
+
 
 formEl.addEventListener('submit', onFormSubmitHandler);
 
-
-function onFormSubmitHandler(evt) {
-    evt.preventDefault();
-    
-    const { searchQuery } = evt.currentTarget.elements;
-    
-    const inputValue = searchQuery.value;
-    console.log(inputValue);
-
-    getImage()
-}
+formEl.addEventListener('click', onLoadMoreHandler);
